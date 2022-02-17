b7fbcc6ce1e936e4e52ca48133c0868c25033016
Sander Pick <sanderpick@gmail.com>
2022-02-17 02:48:37 -0700

    fix up header footer links
    Signed-off-by: Sander Pick <sanderpick@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         <div class="py-4">
           <img src="~assets/img/logo-black.svg" alt="Tableland" class="h-5">
         </div>
-        <ul class="flex flex-1 justify-end items-center gap-x-8 md:gap-x-10 lg:gap-x-24 uppercase text-sm">
+        <ul class="flex flex-1 justify-end items-center gap-x-3 sm:gap-x-6 md:gap-x-10 lg:gap-x-24 uppercase text-sm ml-3">
           <li><a href="https://tableland.substack.com">Blog</a></li>
           <li><a href="https://discord.gg/7WDqyanQ" target="_blank">Discord</a></li>
           <li><a href="https://docs.tableland.xyz">Docs</a></li>
@@ -250,14 +250,14 @@
         <div class="hidden py-4">
           <img src="~assets/img/logo-white.svg" alt="Tableland" class="h-5">
         </div>
-        <ul class="flex justify-center items-center gap-x-3 sm:gap-x-6 md:gap-x-12 xl:gap-x-24 uppercase text-sm">
-          <li><a href="https://twitter.com/tableland__">Twitter</a></li>
+        <ul class="flex justify-center items-center gap-x-3 sm:gap-x-6 md:gap-x-12 xl:gap-x-24 uppercase text-xs">
+          <li class="hidden md:inline-block"><a href="https://twitter.com/tableland__">Twitter</a></li>
           <li><a href="https://textile.notion.site/Tableland-Grants-Funding-ebc1f398d53a481d94f090ab12d93be0">Grants</a></li>
-          <li><a href="https://boards.greenhouse.io/textileio">Jobs</a></li>
+          <li class="hidden md:inline-block"><a href="https://boards.greenhouse.io/textileio">Jobs</a></li>
           <li><a href="https://hhueol4i6vp.typeform.com/to/sgtDW2Xt">Token Info</a></li>
           <li><a href="https://docs.tableland.xyz/general/community/partners">Partners</a></li>
-          <li><a href="https://textile.notion.site/Tableland-Privacy-Policy-6fd160e7f485491d9dc4cbab188043d5">Privacy Policy</a></li>
-          <li><a href="https://textile.notion.site/Tableland-Terms-of-Use-cf80f1b550b843ad9d4b8c3140b78e35">Terms of Use</a></li>
+          <li><a href="https://textile.notion.site/Tableland-Privacy-Policy-6fd160e7f485491d9dc4cbab188043d5">Privacy</a></li>
+          <li><a href="https://textile.notion.site/Tableland-Terms-of-Use-cf80f1b550b843ad9d4b8c3140b78e35">Terms</a></li>
         </ul>
       </nav>
     </footer>
