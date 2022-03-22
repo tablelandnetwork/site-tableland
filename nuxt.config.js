8691cc5aecf2f8a142914ebf892705a85dc33f8a
Erik Zuuring <erik@circlesphere.co>
2022-03-22 10:20:36 +0200

    Animation implementations
    - Added scroll / animation events package

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,7 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [{ src: "~/plugins/aos", ssr: false }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
