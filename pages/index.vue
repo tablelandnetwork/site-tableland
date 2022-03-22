0c7ecd24147a3a4103efef2339a51ece6e2d5bee
Erik Zuuring <erik@circlesphere.co>
2022-03-22 13:58:20 +0200

    Guild + animations
    - Adding guild sections
    - Added scroll animations to home + pre-mint page

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div>
 
     <!-- Header -->
-    <header class="bg-navbar">
+    <header class="bg-navbar" data-aos="fade-down" data-aos-anchor=".leading-snug">
       <nav class="container px-6 md:px-9 lg:px-16 flex items-center py-2">
         <div class="py-4">
           <img src="~assets/img/logo-black.svg" alt="Tableland" class="h-5">
@@ -18,22 +18,32 @@
 
     <!-- hero -->
     <section class="hero">
+      <nav class="container px-6 md:px-9 lg:px-16 flex items-center py-2 pt-10">
+        <div class="py-4">
+          <img src="~assets/img/logo-black.svg" alt="Tableland" class="h-5">
+        </div>
+        <ul class="flex flex-1 justify-end items-center gap-x-3 sm:gap-x-6 md:gap-x-10 lg:gap-x-12 uppercase ml-3">
+          <li><a href="https://mirror.xyz/tableland.eth">Blog</a></li>
+          <li><a href="https://discord.gg/hpd5WWn4Ys" target="_blank">Discord</a></li>
+          <li><a href="https://docs.tableland.xyz">Docs</a></li>
+        </ul>
+      </nav>
       <div class="container px-6 md:px-9 lg:px-16 flex items-center pt-18 lg:pt-36">
         <div class="lg:w-2/3">
-          <h1 class="font-Orbitron text-4xl md:text-5xl lg:text-6xl xl:text-7xl leading-tighter mb-4 lg:mb-10">
+          <h1 class="font-Orbitron text-4xl md:text-5xl lg:text-6xl xl:text-7xl leading-tighter mb-4 lg:mb-10" data-aos="fade-up">
             Build web3 with <span class="font-black">SQL</span>
           </h1>
-          <p class="text-xl md:text-3xl leading-normal md:leading-normal font-light mb-10 lg:mb-18">
+          <p class="text-xl md:text-3xl leading-normal md:leading-normal font-light mb-10 lg:mb-18" data-aos="fade-up">
             Tableland is a network and relational metadata protocol for EVM chains like Ethereum.
             Built for developers, NFT creators, and web3 visionaries.
           </p>
-          <div class="hero-btn hidden md:inline-block text-center md:text-left mb-18 lg:mb-48">
+          <div class="hero-btn hidden md:inline-block text-center md:text-left mb-18 lg:mb-48" data-aos="fade-up">
             <ButtonLink href="https://docs.tableland.xyz">Get started</ButtonLink>
           </div>
           <div class="md:hidden text-center md:text-left">
             <ButtonLink href="https://discord.gg/hpd5WWn4Ys">Discord</ButtonLink>
           </div>
-          <p class="hidden md:inline-block text-2xl md:text-3xl lg:text-4.25xl leading-snug md:leading-snug font-medium">
+          <p class="hidden md:inline-block text-2xl md:text-3xl lg:text-4.25xl leading-snug md:leading-snug font-medium" data-aos="fade-up">
             Use familiar SQL tables to read and write dynamic NFT metadata or application
             data–all orchestrated by token networks like Ethereum.
           </p>
@@ -45,22 +55,22 @@
     <section class="garage">
       <div class="container px-6 md:px-9 lg:px-16 flex items-center">
         <div class="lg:w-2/3">
-          <h3 class="font-Orbitron text-white text-4xl md:text-5xl lg:text-6xl leading-tighter mb-10 md:mb-4 lg:mb-10">
+          <h3 class="font-Orbitron text-white text-4xl md:text-5xl lg:text-6xl leading-tighter mb-10 md:mb-4 lg:mb-10" data-aos="fade-up">
             Welcome to Tableland
           </h3>
-          <p class="text-white text-xl md:text-2xl lg:text-3xl leading-normal md:leading-normal lg:leading-normal font-light mb-2">
+          <p class="text-white text-xl md:text-2xl lg:text-3xl leading-normal md:leading-normal lg:leading-normal font-light mb-2" data-aos="fade-up">
             1—Use tables to store mutable, partially mutable, or immutable NFT metadata.
           </p>
-          <p class="text-white text-xl md:text-2xl lg:text-3xl leading-normal md:leading-normal lg:leading-normal font-light mb-2">
+          <p class="text-white text-xl md:text-2xl lg:text-3xl leading-normal md:leading-normal lg:leading-normal font-light mb-2" data-aos="fade-up">
             2—Use tables to attach in-app or in-game metadata to existing NFTs.
           </p>
-          <p class="text-white text-xl md:text-2xl lg:text-3xl leading-normal md:leading-normal lg:leading-normal font-light mb-2">
+          <p class="text-white text-xl md:text-2xl lg:text-3xl leading-normal md:leading-normal lg:leading-normal font-light mb-2" data-aos="fade-up">
             3—Control table write access with token or address-based rules.
           </p>
-          <p class="text-white text-xl md:text-2xl lg:text-3xl leading-normal md:leading-normal lg:leading-normal font-light mb-10 lg:mb-18">
+          <p class="text-white text-xl md:text-2xl lg:text-3xl leading-normal md:leading-normal lg:leading-normal font-light mb-10 lg:mb-18" data-aos="fade-up">
             4—Use tables to build fully decentralized apps and games that require complex relational data models.
           </p>
-          <div class="text-center md:text-left">
+          <div class="text-center md:text-left" data-aos="fade-up">
             <ButtonLink href="https://docs.tableland.xyz/building/examples/playbooks">See playbooks</ButtonLink>
           </div>
         </div>
@@ -72,17 +82,17 @@
       <div class="garage-break"></div>
       <div class="container px-6 md:px-9 lg:px-16 flex pt-10 xl:pt-14">
         <div class="lg:w-2/3 xl:w-1/2">
-          <h3 class="font-Orbitron text-4xl md:text-5xl lg:text-6xl leading-tighter mb-4 lg:mb-10">
+          <h3 class="font-Orbitron text-4xl md:text-5xl lg:text-6xl leading-tighter mb-4 lg:mb-10" data-aos="fade-up">
             In Tableland, we speak SQL
           </h3>
-          <p class="text-xl md:text-3xl leading-normal md:leading-normal font-light mb-2">
+          <p class="text-xl md:text-3xl leading-normal md:leading-normal font-light mb-2" data-aos="fade-up">
             1–The bulk of digital information is stored in relational tables.
           </p>
-          <p class="text-xl md:text-3xl leading-normal md:leading-normal font-light mb-2">
+          <p class="text-xl md:text-3xl leading-normal md:leading-normal font-light mb-2" data-aos="fade-up">
             2–Your familiar SQL tables are stored on a decentralized network of validators
             and anchored to Ethereum as transferable NFTs, providing ownership and access control.
           </p>
-          <p class="text-xl md:text-3xl leading-normal md:leading-normal font-light">
+          <p class="text-xl md:text-3xl leading-normal md:leading-normal font-light" data-aos="fade-up">
             3–Easy as pie, right?
           </p>
         </div>
@@ -94,13 +104,13 @@
       <section class="playground text-white pt-6 md:pt-32 pb-32">
         <div class="container px-6 md:px-9 lg:px-16 flex justify-center">
           <div class="md:w-2/3">
-            <h3 class="font-Orbitron text-4xl md:text-5xl lg:text-6xl leading-tighter mb-4 lg:mb-10">
+            <h3 class="font-Orbitron text-4xl md:text-5xl lg:text-6xl leading-tighter mb-4 lg:mb-10" data-aos="fade-up">
               Try it out
             </h3>
-            <p v-if="ethereum" class="text-xl md:text-3xl leading-normal md:leading-normal font-light mb-4 lg:mb-6">
+            <p v-if="ethereum" class="text-xl md:text-3xl leading-normal md:leading-normal font-light mb-4 lg:mb-6" data-aos="fade-up">
               Go ahead! Don't take our word for it.
             </p>
-            <p v-if="!ethereum" class="text-xl md:text-3xl leading-normal md:leading-normal font-light mb-4 lg:mb-6">
+            <p v-if="!ethereum" class="text-xl md:text-3xl leading-normal md:leading-normal font-light mb-4 lg:mb-6" data-aos="fade-up">
               Uh oh! You need a browser wallet. Go install <a href="https://metamask.io/" target="_blank" class="underline">Metamask</a> or use a browser with a built-in wallet like <a href="https://brave.com/" target="_blank" class="underline">Brave</a>.
             </p>
             <playground></playground>
@@ -114,24 +124,24 @@
       <div class="container px-6 md:px-9 lg:px-16">
         <div class="community-top pt-32">
           <div class="md:w-2/3 xl:w-1/2">
-            <h3 class="font-Orbitron text-4xl md:text-5xl lg:text-6xl leading-tighter mb-4 lg:mb-10">
+            <h3 class="font-Orbitron text-4xl md:text-5xl lg:text-6xl leading-tighter mb-4 lg:mb-10" data-aos="fade-up">
               Join the community
             </h3>
-            <p class="text-xl md:text-3xl leading-normal md:leading-normal font-light mb-10 lg:mb-18">
+            <p class="text-xl md:text-3xl leading-normal md:leading-normal font-light mb-10 lg:mb-18" data-aos="fade-up">
               Whether you're a developer, NFT creator, token holder, or just curious about web3, there's a place for you in Tableland. Join a fast-growing community of innovators from all over the world.
             </p>
-            <div class="text-center md:text-left">
+            <div class="text-center md:text-left" data-aos="fade-up">
               <ButtonLink href="https://discord.gg/hpd5WWn4Ys">Join us on Discord</ButtonLink>
             </div>
           </div>
         </div>
 
-        <div class="community-bottom md:flex justify-center lg:justify-end text-white">
+        <div class="community-bottom md:flex justify-center lg:justify-end text-white" >
           <div>
-            <h3 class="font-Orbitron text-4xl md:text-5xl lg:text-6xl leading-tighter md:leading-tighter lg:leading-tighter mb-4 lg:mb-10">
+            <h3 class="font-Orbitron text-4xl md:text-5xl lg:text-6xl leading-tighter md:leading-tighter lg:leading-tighter mb-4 lg:mb-10" data-aos="fade-up">
               User-owned token networks are the future of the internet
             </h3>
-            <p class="text-xl md:text-3xl leading-normal md:leading-normal font-light">
+            <p class="text-xl md:text-3xl leading-normal md:leading-normal font-light" data-aos="fade-up">
               Web3 needs a relational and queryable metadata layer. Tableland is a decentralized user-owned network for structured metadata—data from apps, games, social, analytics, AI/ML, or whatever else you can imagine. Tableland tables are also NFTs, making them own-able, tradable, and composable assets themselves.
             </p>
           </div>
@@ -141,11 +151,11 @@
     </section>
 
     <!-- showcase -->
-    <section class="md:block showcase py-32 relative">
+    <section class="md:block showcase py-32 relative" >
       <div class="container px-6 md:px-9 lg:px-16">
         <div class="text-center text-white">
-          <p class="text-3xl md:text-4xl lg:text-5xl md:leading-normal lg:leading-normal font-light mb-10">What will you build?</p>
-          <ButtonLink href="https://textile.notion.site/Tableland-Use-cases-466b4ab8bb1248a497dd457f057abf5f">See use cases</ButtonLink>
+          <p class="text-3xl md:text-4xl lg:text-5xl md:leading-normal lg:leading-normal font-light mb-10" data-aos="fade-up">What will you build?</p>
+          <ButtonLink href="https://textile.notion.site/Tableland-Use-cases-466b4ab8bb1248a497dd457f057abf5f" data-aos="fade-up">See use cases</ButtonLink>
         </div>
       </div>
     </section>
