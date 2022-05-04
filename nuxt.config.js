05bfaa5d9ef80b81341b8bbeb7c0962445025ff2
Erik Zuuring <erik@circlesphere.co>
2022-05-04 08:37:24 -0400

    OG fix attempt 2
    - Trying to change hooks for scrappers

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
   ssr: false,
 
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'server',
+  target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -80,5 +80,11 @@ export default {
     options: {
            fix: true
          }
-  }
+  },
+
+  hooks: {
+    'generate:page': page => {
+      page.html = page.html.replace(/ data-n-head=".*?"/gi, '').replace(/ data-hid=".*?"/gi, '')
+    },
+  },
 };
