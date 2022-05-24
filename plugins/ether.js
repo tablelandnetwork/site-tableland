98ac4c83732dcdb86be853101f7e1998bac17c14
Erik Zuuring <erik@circlesphere.co>
2022-05-24 16:50:20 +0300

    Further minter updates
    - Setup metamask flow with contract interaction
    - Still some bugs to sort out with connecting properly to contract and minting as transaction failed

--- plugins/ether.js
+++ plugins/ether.js
@@ -3,7 +3,17 @@ import { ethers } from 'ethers'
 
 export default async ({env}, inject) => {
 
-    const nftContractAddress = "0x2acb10fb88268d3cfc5ee4fcf8b37d8e63e4167d"
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+
+    const rig = {
+        address: "0xB70C91df903AC3F2A3aD0BeabFeC3fe89dc69c99",
+        abi: [
+          "function gimmeSome() external",
+          "function balanceOf(address _owner) public view returns (uint256 balance)",
+          "function transfer(address _to, uint256 _value) public returns (bool success)",
+        ],
+      };
 
     const wallet = Vue.observable({
         account: null,
@@ -42,7 +52,10 @@ export default async ({env}, inject) => {
         },
 
         async connect() {
-
+          const provider = new ethers.providers.Web3Provider(
+              window.ethereum,
+              "any"
+            );
             wallet.network = wallet.provider.getNetwork()
 
             const [account] = await wallet.provider.send('eth_requestAccounts')
@@ -51,44 +64,42 @@ export default async ({env}, inject) => {
             if(account) {
                 await wallet.setAccount(account)
             }
-            
+            await provider.send("eth_requestAccounts", []);
+            const signer = provider.getSigner();
         },
 
-        async mint() {
+        async mintRig() {
           try {
            const { ethereum } = window
 
            if (ethereum) {
-               const provider = new ethers.providers.Web3Provider(ethereum)
-               const signer = provider.getSigner()
-               const nftContract = new ethers.Contract(
-                   nftContractAddress,
-                   // NFT.abi,
-                   signer
-               )
 
-               let nftTx = await nftContract.createEternalNFT()
-               console.log('Minting rig...', nftTx.hash)
-               setMiningStatus(0)
+          document.getElementById("mint-button").innerHTML="Confirm the Transaction";
+          await provider.send("eth_requestAccounts", []);
+          const signer = provider.getSigner();
+          let userAddress = await signer.getAddress();
+          const nftContract = new ethers.Contract(rig.address, rig.abi, signer);
 
-               let tx = await nftTx.wait()
-               setLoadingState(1)
-               console.log('minted rig!!', tx)
-               let event = tx.events[0]
-               let value = event.args[2]
-               let tokenId = value.toNumber()
+          const tx = await nftContract.gimmeSome({ gasPrice: 1e9, gasLimit: 80000 });
+          console.log = function(message) {
+          document.getElementById('mint-log').innerHTML = message;
+          };
+          console.log(`Transaction hash: ${tx.hash}`);
+          document.getElementById("mint-button").innerHTML="Minting in Progress";
 
-               console.log(
-                   `Minted your rig, see transaction: https://rinkeby.etherscan.io/tx/${nftTx.hash}`
-               )
+          const receipt = await tx.wait();
+          console.log(`Transaction confirmed in block ${receipt.blockNumber}`);
+          console.log(`Gas used: ${receipt.gasUsed.toString()}`);
+          console.log(
+              `Minted your rig, see transaction: https://rinkeby.etherscan.io/tx/${tx.hash}`
+          );
 
-               getMintedNFT(tokenId)
-           } else {
-               console.log("object doesnt exist!")
-           }
-       } catch (error) {
-           console.log('Error minting rig', error)
-       }
+          } else {
+              console.log("object doesnt exist!")
+          }
+      } catch (error) {
+          console.log('Error minting rig', error)
+      }
 
         },
 
