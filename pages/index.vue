704dc691728e109d3e1e4c8639b0e1885ef03fbc
Sander Pick <sanderpick@gmail.com>
2022-05-31 10:08:04 -0700

    fade up rigs preview on home
    Signed-off-by: Sander Pick <sanderpick@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -40,7 +40,7 @@
             <h1 class="font-Orbitron text-4xl md:text-5xl leading-tighter mb-4 lg:mb-10 choose" data-aos="fade-up">
               Grab your <span class="font-black">Rig</span>
             </h1>
-            <img src="~assets/img/main_rigs.png" class="rounded-lg" />
+            <img src="~assets/img/main_rigs.png" class="rounded-lg" data-aos="fade-up" />
           </div>
         </div>
         <div class="px-6 md:px-9 lg:px-16 flex">
