53b2c4950a6b100f933dae96f34c20d93dbee8fc
Andrew Hill <andrew@textile.io>
2022-06-15 13:21:04 -0700

    fix(footer): more recent links
    Signed-off-by: Andrew Hill <andrew@textile.io>

--- pages/pilots.vue
+++ pages/pilots.vue
@@ -137,10 +137,9 @@
         </div>
         <ul class="flex justify-center items-center gap-x-3 sm:gap-x-6 md:gap-x-12 xl:gap-x-24 uppercase text-xs">
           <li class="hidden md:inline-block"><a href="https://twitter.com/tableland__">Twitter</a></li>
-          <li><a href="https://textile.notion.site/Tableland-Grants-Funding-ebc1f398d53a481d94f090ab12d93be0">Grants</a></li>
           <li class="hidden md:inline-block"><a href="https://boards.greenhouse.io/textileio">Jobs</a></li>
-          <li><a href="https://hhueol4i6vp.typeform.com/to/sgtDW2Xt">Token Info</a></li>
-          <li><a href="https://docs.tableland.xyz/general/community/partners">Partners</a></li>
+          <li><a href="https://tableland.xyz/rigs">Rigs</a></li>
+          <li><a href="https://docs.tableland.xyz/">Docs</a></li>
           <li><a href="https://textile.notion.site/Tableland-Privacy-Policy-6fd160e7f485491d9dc4cbab188043d5">Privacy</a></li>
           <li><a href="https://textile.notion.site/Tableland-Terms-of-Use-cf80f1b550b843ad9d4b8c3140b78e35">Terms</a></li>
         </ul>
