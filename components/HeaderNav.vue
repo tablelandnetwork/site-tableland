adfb8317a9577a072bcd4cb8843af60cfbfa7402
Erik Zuuring <erik@circlesphere.co>
2022-06-16 19:19:08 +0300

    Mobile nav fix
    - Improved mobile nav animations

--- components/HeaderNav.vue
+++ components/HeaderNav.vue
@@ -1,36 +1,20 @@
 <template>
-  <div
-    class="nav-bar"
-    :class="{ 'absolute w-full h-screen top-0 left-0': menu }"
-  >
+  <div class="nav-bar" :class="{ 'absolute w-full h-screen top-0 left-0': menu }">
     <div class="sm:mx-auto flex flex-wrap">
+
       <!-- Desktop nav -->
-      <nav
-        class="container hidden md:inline-flex justify-between px-6 md:px-9 lg:px-16 flex items-center py-2 pt-6"
-      >
+      <nav class="container hidden md:inline-flex justify-between px-6 md:px-9 lg:px-16 flex items-center py-2 pt-6">
         <div class="py-4">
-          <a href="/"
-            ><img src="~assets/img/logo-black.svg" alt="Tableland" class="h-5"
-          /></a>
+          <a href="/"><img src="~assets/img/logo-black.svg" alt="Tableland" class="h-5"></a>
         </div>
         <div class="py-4">
-          <ul
-            class="flex justify-end items-center gap-x-3 sm:gap-x-6 md:gap-x-10 lg:gap-x-12 uppercase ml-3"
-          >
+          <ul class="flex justify-end items-center gap-x-3 sm:gap-x-6 md:gap-x-10 lg:gap-x-12 uppercase ml-3">
             <li v-for="(item, index) in titles" :key="index">
-              <a
-                v-if="index != titles.length - 1"
-                :href="hrefs[index]"
-                :target="targets[index] ? targets[index] : ''"
-              >
+              <a v-if="index != titles.length - 1" :href="hrefs[index]" :target="targets[index] ? targets[index] : ''">
                 {{ item }}
               </a>
-              <a
-                v-else
-                :href="hrefs[index]"
-                :target="targets[index] ? targets[index] : ''"
-                class="btn bg-black text-white"
-              >
+              <a v-else :href="hrefs[index]" :target="targets[index] ? targets[index] : ''"
+                class="btn bg-black text-white">
                 {{ item }}
               </a>
             </li>
@@ -40,42 +24,35 @@
 
       <!-- Toggle menu mobile -->
       <div class="py-12 px-6 md:hidden logo">
-        <a href="/"
-          ><img src="~assets/img/logo-black.svg" alt="Tableland" class="h-4"
-        /></a>
+        <a href="/"><img src="~assets/img/logo-black.svg" alt="Tableland" class="h-4"></a>
       </div>
-      <button class="ml-auto md:hidden p-6" @click="menu = !menu">MENU</button>
+      <button class="ml-auto md:hidden p-6" @click="menu = !menu">
+        {{ !!menu ? 'CLOSE' : 'MENU' }}
+      </button>
     </div>
 
     <!-- Mobile nav  -->
-    <nav
-      v-show="menu"
-      data-aos="fade-down"
-      class="aos-animate w-full"
-      style="background-color: #815691; transition: all 0.2s ease-in-out"
-    >
-      <ul class="flex flex-col text-center">
-        <li v-for="(item, index) in titles" :key="index">
-          <a
-            :href="hrefs[index]"
-            :target="targets[index] ? targets[index] : ''"
-            class="p-6 block"
-          >
-            {{ item }}
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <transition name="mobile">
+      <nav v-show="menu" class="w-full mobile-nav">
+        <ul class="flex flex-col text-center">
+          <li v-for="(item, index) in titles" :key="index">
+            <a :href="hrefs[index]" :target="targets[index] ? targets[index] : ''" class="p-6 block text-l">
+              {{ item }}
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </transition>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["titles", "hrefs", "targets"],
-  data() {
+  props: ['titles', 'hrefs', 'targets'],
+  data: function () {
     return {
-      menu: false,
+      menu: false
     };
-  },
+  }
 };
 </script>
