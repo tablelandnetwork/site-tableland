8da630134292a659f854d8317e6eb4d29b0a4f6a
Erik Zuuring <erik@circlesphere.co>
2022-06-17 18:26:09 +0300

    No wallet fix

--- components/HeaderNav.vue
+++ components/HeaderNav.vue
@@ -69,8 +69,7 @@
                     >
                   </li>
                   <li>
-                    <a
-                      @click="$wallet.disconnect"
+                    <a 
                       class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                       >Disconnect</a
                     >
