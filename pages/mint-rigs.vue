4021a06427b14839a96b58605bb3ec27b902099e
Sander Pick <sanderpick@gmail.com>
2022-07-14 09:15:25 -0700

    basic minter
    Signed-off-by: Sander Pick <sanderpick@gmail.com>

---
+++ pages/mint-rigs.vue
@@ -0,0 +1,184 @@
+<template>
+  <div class="mint-page mint-rigs">
+    <HeaderNav :titles="nav.map((i) => i.title)" :hrefs="nav.map((i) => i.href)" :targets="nav.map((i) => i.target)" />
+    <HeaderScrollNav anchor=".choose" :titles="nav.map((i) => i.title)" :hrefs="nav.map((i) => i.href)"
+      :targets="nav.map((i) => i.target)" />
+    <!-- hero -->
+    <section class="rigs-hero">
+      <div class="container px-0 sm:px-6 md:px-12 pt-12 sm:pt-24 lg:pt-32 xl:pt-48 justify-between">
+        <div class="w-full px-6 pb-6 lg:pb-10 pt-12" data-aos="fade-up">
+          <div class="w-full pt-6">
+            <h1 class="text-white w-full h-auto font-Orbitron text-5xl lg:text-6xl xl:text-7xl leading-tighter">
+              Mint <span class="font-black">Rigs</span>
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div class="container px-0 sm:px-6 md:px-12 pt-0 lg:pt-0 pb-24" data-aos="fade-up">
+        <div v-if="!address" class="w-full md:w-full lg:w-1/2 xl:w-1/2 px-6 pb-0 lg:pb-0 pt-0">
+          <h1 class="text-white w-full h-auto text-xl md:text-2xl xl:text-2xl leading-tighter mb-10 lg:mb-18">
+            Each Rig is generated from 1,074 handcrafted works of art for the
+            builders and creatives of cyberspace. It's time to grab yours.
+          </h1>
+        </div>
+        <div v-else class="w-full md:w-full lg:w-1/2 xl:w-1/2 px-6 pb-0 lg:pb-0 pt-0">
+          <h1 class="text-white w-full h-auto text-xl md:text-2xl xl:text-2xl leading-tighter mb-10 lg:mb-18">
+            Each Rig is generated from 1,074 handcrafted works of art for the
+            builders and creatives of cyberspace. It's time to grab yours.
+          </h1>
+          <h1 class="text-white w-full h-auto text-xl md:text-2xl xl:text-2xl leading-tighter mb-10 lg:mb-18">
+            Hey Tablelander! Looks like you can grab {{ (paidAllowance + freeAllowance) - existing }} Rig(s) 0.05E each
+            + gas.
+          </h1>
+        </div>
+        <div class="w-full px-6 pb-0 lg:pb-0 pt-0">
+          <a v-if="!address" class="btn bg-black text-white" @click="connect">
+            <span class="flex">
+              Connect Wallet
+              <img src="~assets/img/arrow_white.png" srcset="
+                  ~assets/img/arrow_white.png    1x,
+                  ~assets/img/arrow_white@2x.png 2x
+                " class="hidden sm:inline-block ml-4" alt="" /> </span></a>
+          <div v-else>
+            <a class="btn bg-black text-white" @click="mint">
+              <span class="flex">
+                Mint
+                <img src="~assets/img/arrow_white.png" srcset="
+                    ~assets/img/arrow_white.png    1x,
+                    ~assets/img/arrow_white@2x.png 2x
+                  " class="hidden sm:inline-block ml-4" alt="" /> </span></a>
+            <input class="inline" v-model.number="quantity" type="number" min="1"
+              :max="freeAllowance + paidAllowance" />
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- <div class="bg-solid">
+      <div class="main min-h-screen container flex flex-col items-center p-8 text-sm">
+        <div ref="grid" class="w-full grid gap-4 grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+          <div v-for="(src, i) in rigs" :key="i">
+            <v-lazy-image v-if="i < 20" :style="{ minHeight: imageHeight }"
+              class="m-auto bg-black dark:bg-white bg-opacity-20 dark:bg-opacity-20" :src="src"
+              @intersect="imageIntersect" @load="imageLoad" />
+            <v-lazy-image v-else :style="{ minHeight: imageHeight }"
+              class="m-auto bg-black dark:bg-white bg-opacity-20 dark:bg-opacity-20 choose" :src="src"
+              @intersect="imageIntersect" @load="imageLoad" />
+          </div>
+        </div>
+      </div>
+    </div> -->
+    <FooterNav />
+  </div>
+</template>
+
+<script>
+// import { mapState } from "vuex";
+
+export default {
+  components: {},
+
+  data() {
+    return {
+      mintstatus: 0,
+      address: undefined,
+      quantity: 1,
+      existing: 0,
+      freeAllowance: 0,
+      paidAllowance: 0,
+      proof: [],
+      nav: [
+        {
+          title: "Discord",
+          href: "https://discord.gg/hpd5WWn4Ys",
+          target: "_blank",
+        },
+        {
+          title: "Blog",
+          href: "https://mirror.xyz/tableland.eth",
+          target: "_blank",
+        },
+        {
+          title: "Docs",
+          href: "https://docs.tableland.xyz",
+          target: "_blank",
+        },
+        {
+          title: "More Info",
+          href: "/rigs",
+          main: true,
+        },
+      ],
+    };
+  },
+
+  head() {
+    return {
+      title: "The Tableland NFT: Grab your Rig",
+      meta: [
+        { hid: "og-url", property: "og:url", content: `${this.$route.path}` },
+        { hid: "og-type", property: "og:type", content: "website" },
+        {
+          hid: "og-title",
+          property: "og:title",
+          content: "The Tableland NFT: Grab your Rig",
+        },
+        {
+          hid: "og-sitename",
+          property: "og:site_name",
+          content: "tableland.xyz/rigs/mint",
+        },
+        {
+          hid: "og-desc",
+          property: "og:description",
+          content:
+            "A 3k generative NFT built from 1,074 handcrafted works of art for the builders and creatives of cyberspace.",
+        },
+        {
+          hid: "og-image",
+          property: "og:image",
+          content: "https://i.imgur.com/5YHLnkC.png",
+        },
+      ],
+    };
+  },
+
+  created() {
+    //
+  },
+
+  mounted() {
+    //
+  },
+
+  destroyed() {
+    removeEventListener("resize", this.resizeImages);
+  },
+
+  methods: {
+    async connect() {
+      const status = await this.$store.dispatch("getRigsStatus");
+      if (status) {
+        this.address = status.address;
+        this.mintstatus = status.mintstatus || 0;
+        this.freeAllowance = status.entry ? status.entry[1] : 0;
+        this.paidAllowance = status.entry ? status.entry[2] : 0;
+        this.proof = status.proof || [];
+        this.existing = status.tokens.length;
+      }
+    },
+
+    async mint() {
+      const receipt = await this.$store.dispatch("mintRigs", {
+        quantity: this.quantity,
+        freeAllowance: this.freeAllowance,
+        paidAllowance: this.paidAllowance,
+        proof: this.proof
+      });
+      console.log(receipt);
+    },
+  },
+};
+</script>
+
+<style scoped src="~/assets/css/samples.css">
+</style>
--- store/index.ts
+++ store/index.ts
@@ -1,6 +1,12 @@
-import { ActionTree, GetterTree, MutationTree } from "vuex";
+import { ActionTree, MutationTree } from "vuex";
 import { connect, ConnectOptions } from "@tableland/sdk";
-import { BigNumber, ethers, utils } from "ethers";
+import { ethers, BigNumber } from "ethers";
+import Web3Modal from "web3modal";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import { TablelandRigs, TablelandRigs__factory } from "@tableland/rigs";
+import { deployments } from "@tableland/rigs/deployments";
+import { MerkleTree } from "merkletreejs";
+import keccak256 from "keccak256";
 
 export const state = function () {
   return {
@@ -31,7 +37,6 @@ export const mutations: MutationTree<RootState> = {
 declare global {
   interface Window {
     tableland: any;
-    rigs: any;
   }
 }
 
@@ -59,15 +64,131 @@ const getConnection = (function () {
   };
 })();
 
-// window.rigs =
-//   "First connect with metamask, then start playing with Rigs!";
-// // store the rigs connection as a private plain Object
-// const getRigs = (function () {
-//   let rigs: any;
-//   return async function () {
-//     ethers.getDefaultProvider()
-//   };
-// })();
+const getRigsProvider = (function () {
+  let web3Modal: Web3Modal | undefined;
+  let provider: ethers.providers.Web3Provider | undefined;
+
+  return async function (options?: any) {
+    if (options?.disconnect) {
+      if (web3Modal) web3Modal.clearCachedProvider();
+      return;
+    }
+    if (provider) return provider;
+
+    const providerOptions = {
+      walletconnect: {
+        package: WalletConnectProvider,
+        options: {
+          infuraId: "2a8924427d074d4c957ed6c03fabc42f",
+        },
+      },
+    };
+    web3Modal = new Web3Modal({
+      cacheProvider: true,
+      providerOptions,
+    });
+    const instance = await web3Modal.connect();
+    provider = new ethers.providers.Web3Provider(instance);
+
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", ([newAddress]: any) => {
+        console.log("accountsChanged", newAddress);
+        if (web3Modal) web3Modal.clearCachedProvider();
+        window.location.reload();
+      });
+
+      window.ethereum.on("chainChanged", (chainId: any) => {
+        console.log("chainChanged", chainId);
+        if (web3Modal) web3Modal.clearCachedProvider();
+        window.location.reload();
+      });
+    }
+
+    return provider;
+  };
+})();
+
+const rigsDeployment = deployments["polygon-mumbai"];
+
+const getRigs = (function () {
+  return async function (provider: ethers.providers.Web3Provider) {
+    const signer = provider.getSigner();
+    return TablelandRigs__factory.connect(
+      rigsDeployment.contractAddress,
+      signer
+    ) as TablelandRigs;
+  };
+})();
+
+const getRigsStatus = (function () {
+  return async function (rigs: TablelandRigs, address: string) {
+    const mintphase = await rigs.mintPhase();
+    console.info("mint phase:", mintphase);
+    const tokens = await rigs.tokensOfOwner(address);
+    console.info("address has:", tokens.length);
+
+    switch (mintphase) {
+      case 0:
+        return { mintphase };
+      case 1:
+      case 2:
+        const tbl = await connect({
+          chain: "ethereum-goerli",
+        } as ConnectOptions);
+
+        const useWaitlist = mintphase === 2 ? 1 : 0;
+        const entryRes = await tbl.read(
+          `select * from ${rigsDeployment.allowlistTable} where address='${address}' and waitlist=${useWaitlist}`
+        );
+        if (entryRes.rows.length === 0) {
+          return { mintphase };
+        }
+        const entry = entryRes.rows[0];
+        console.info("user entry:", entry);
+
+        const list = await tbl.read(
+          `select * from ${rigsDeployment.allowlistTable} where waitlist=${useWaitlist}`
+        );
+        const tree = buildTree(list.rows);
+        const proof = tree.getHexProof(hashEntry(entry));
+        console.info("user proof:", proof);
+
+        return { mintphase, tokens, rigs, entry, proof };
+      default:
+        return { mintphase, rigs };
+    }
+  };
+})();
+
+const mintRigs = (function () {
+  return async function (
+    rigs: TablelandRigs,
+    quantity: number,
+    freeAllowance: number,
+    paidAllowance: number,
+    proof: string[]
+  ) {
+    const tx = await rigs["mint(uint256,uint256,uint256,bytes32[])"](
+      quantity,
+      freeAllowance,
+      paidAllowance,
+      proof
+    );
+    const receipt = await tx.wait();
+    let ids = [];
+    if (receipt.events && receipt.events.length > 0) {
+      for (let i = 0; i < receipt.events.length; i++) {
+        const e = receipt.events[i];
+        if (e.event === "Transfer") {
+          console.log(e.args);
+          const id = (e.args?.tokenId as BigNumber).toNumber();
+          ids.push(id);
+        }
+      }
+    }
+    return ids;
+  };
+})();
 
 export const actions: ActionTree<RootState, RootState> = {
   connect: async function (context) {
@@ -112,6 +233,39 @@ export const actions: ActionTree<RootState, RootState> = {
     const tableland = await getConnection();
     return await tableland.list();
   },
+  getRigsStatus: async function (context) {
+    const provider = await getRigsProvider();
+    if (provider) {
+      try {
+        const rigs = await getRigs(provider);
+        const address = await provider?.getSigner().getAddress();
+        console.info("connnected to", address);
+
+        const status = await getRigsStatus(rigs, address);
+        return { ...status, address };
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  },
+  mintRigs: async function (context, args) {
+    try {
+      const provider = await getRigsProvider();
+      if (provider) {
+        const rigs = await getRigs(provider);
+        const ids = await mintRigs(
+          rigs,
+          args.quantity,
+          args.freeAllowance,
+          args.paidAllowance,
+          args.proof
+        );
+        return ids;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
 
 // RPC responds with rows and columns in separate arrays, this will combine to an array of objects
@@ -137,3 +291,25 @@ const wait = function (ms: number) {
     setTimeout(() => resolve(void 0), ms);
   });
 };
+
+function buildTree(allowlist: [string, number, number, number][]): MerkleTree {
+  return new MerkleTree(
+    allowlist.map((entry) => hashEntry(entry)),
+    keccak256,
+    {
+      sort: true,
+    }
+  );
+}
+
+function hashEntry(entry: [string, number, number, number]): Buffer {
+  return Buffer.from(
+    ethers.utils
+      .solidityKeccak256(
+        ["address", "uint256", "uint256"],
+        [entry[0], entry[1], entry[2]]
+      )
+      .slice(2),
+    "hex"
+  );
+}
