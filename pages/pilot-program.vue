e712c94952846f1cf1dde322e93184aacc9f7e18
Erik Zuuring <erik@circlesphere.co>
2022-08-11 12:29:01 +0300

    Responsive fixes
    - Tweaked pilot program page to have responsive fixes with images and scaling.

--- pages/pilot-program.vue
+++ pages/pilot-program.vue
@@ -22,7 +22,7 @@
     <!-- cohort -->
     <section class="cohort">
       <div
-        class="container px-6 sm:px-6 md:px-24 lg:px-42 flex items-left pt-48 lg:pt-48"
+        class="container px-6 sm:px-6 md:px-24 lg:px-42 flex items-left pt-64 lg:pt-64"
       >
         <div class="lg:w-2/3 md:w-full" data-aos="fade-up" style="margin: auto">
           <h1
@@ -66,7 +66,7 @@
           <div
             data-aos="fade-up"
             data-aos-anchor=".rig"
-            class="w-1/3"
+            class="w-1/3 hidden lg:block"
             style="
               margin-top: -700px;
               overflow: auto;
@@ -78,7 +78,7 @@
           </div>
         </div>
         <div class="lg:flex lg:py-12">
-          <div class="lg:w-1/2 w-full lg:mt-48">
+          <div class="lg:w-1/2 w-full lg:mt-48 mt-24">
             <img src="~assets/img/wall2.png" />
           </div>
           <div class="lg:w-1/2 px-12 py-12 lg:py-32 lg:mt-32">
@@ -116,8 +116,6 @@
           </div>
         </div>
       </div>
-
-      <div class="rig-break-bottom"></div>
     </section>
 
     <section id="why" class="cohort-info">
@@ -132,8 +130,8 @@
             </h1>
           </div>
         </div>
-        <div class="lg:flex py-12 pb-12 text-center text-white">
-          <div class="lg:w-1/3 w-full px-12 py-0">
+        <div class="grid grid-cols-1 lg:grid-cols-2 justify-center place-content-center py-12 pb-12 lg:px-48 text-center text-white">
+          <div class="lg:px-12 py-0">
             <img
               class="icon py-12"
               src="~assets/img/wrench2.png"
@@ -152,7 +150,7 @@
               developers at the frontier.
             </p>
           </div>
-          <div class="lg:w-1/3 w-full px-12 py-0">
+          <div class="lg:px-12 py-0">
             <img
               class="icon py-12"
               src="~assets/img/chip2.png"
@@ -170,7 +168,7 @@
               project become real.
             </p>
           </div>
-          <div class="lg:w-1/3 w-full px-12 py-0">
+          <div class="lg:px-12 py-0">
             <img
               class="icon py-12"
               src="~assets/img/match2.png"
@@ -186,9 +184,7 @@
               3) to continue building and collaborating on the network.
             </p>
           </div>
-        </div>
-        <div class="lg:flex lg:px-48 py-0 pb-12 text-center text-white">
-          <div class="lg:w-1/2 w-full px-12 py-0">
+          <div class="lg:px-12 py-0">
             <img
               class="icon py-12"
               src="~assets/img/wrench2.png"
@@ -206,7 +202,7 @@
               peers.
             </p>
           </div>
-          <div class="lg:w-1/2 w-full px-12 py-0">
+          <div class="lg:px-12 py-0">
             <img
               class="icon py-12"
               src="~assets/img/chip2.png"
@@ -249,43 +245,7 @@
         </div>
       </div>
     </section>
-
-    <footer class="text-blue py-10">
-      <nav class="container px-6 md:px-9 lg:px-16 py-2">
-        <div class="hidden py-4">
-          <img
-            id="js-scroll"
-            src="~assets/img/logo-white.svg"
-            alt="Tableland"
-            class="h-5"
-          />
-        </div>
-        <ul
-          class="flex justify-center items-center gap-x-3 sm:gap-x-6 md:gap-x-12 xl:gap-x-24 uppercase text-xs"
-        >
-          <li class="hidden md:inline-block">
-            <a href="https://twitter.com/tableland__">Twitter</a>
-          </li>
-          <li class="hidden md:inline-block">
-            <a href="https://boards.greenhouse.io/textileio">Jobs</a>
-          </li>
-          <li><a href="https://tableland.xyz/rigs">Rigs</a></li>
-          <li><a href="https://docs.tableland.xyz/">Docs</a></li>
-          <li>
-            <a
-              href="https://textile.notion.site/Tableland-Privacy-Policy-6fd160e7f485491d9dc4cbab188043d5"
-              >Privacy</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://textile.notion.site/Tableland-Terms-of-Use-cf80f1b550b843ad9d4b8c3140b78e35"
-              >Terms</a
-            >
-          </li>
-        </ul>
-      </nav>
-    </footer>
+    <FooterNav />
   </div>
 </template>
 
