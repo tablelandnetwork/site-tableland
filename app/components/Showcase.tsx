b0041df5904cc5a00954c1dc00f3ff7256c50014
Sander Pick <sanderpick@gmail.com>
2023-04-10 23:34:35 -0700

    initial migration to nextjs
    Signed-off-by: Sander Pick <sanderpick@gmail.com>

---
+++ app/components/Showcase.tsx
@@ -0,0 +1,27 @@
+import ButtonLink from "./ButtonLink"
+import { ReactNode } from "react"
+
+type Props = {
+  href: string
+  classes?: string
+  children: ReactNode
+}
+
+export default function Showcase({ href, classes, children }: Props) {
+  return (
+    <section className={"text-neongreen border-b " + classes}>
+      <div className="w-full h-[18px] bg-white opacity-20"></div>
+      <div className="py-12 md:py-32">
+        <div className="text-center">
+          <ButtonLink
+            href={href}
+            target="_blank"
+            classes="text-white bg-lightgreen"
+          >
+            {children}
+          </ButtonLink>
+        </div>
+      </div>
+    </section>
+  )
+}
---
+++ lib/getRigs.ts
@@ -0,0 +1,39 @@
+export function getRigs(count: number, fleet?: string): string[] {
+  let indexes: number[] = []
+  let max = 100
+  const path = fleet || "all"
+  if (!fleet) {
+    max = 1000
+  }
+  if (count > max) {
+    return []
+  }
+  if (count === max) {
+    indexes = shuffle([...Array(count).keys()])
+  } else {
+    while (indexes.length < count) {
+      const j = Math.floor(Math.random() * max)
+      if (!indexes.includes(j)) {
+        indexes.push(j)
+      }
+    }
+  }
+  return indexes.map((v) => {
+    return "/img/rigs/samples/" + path + "/" + v + ".jpg"
+  })
+}
+
+// https://stackoverflow.com/a/2450976/641834
+function shuffle(array: number[]) {
+  let currentIndex = array.length
+  let randomIndex
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex--
+    ;[array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ]
+  }
+  return array
+}
