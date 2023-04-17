0930e4b870e5ffc14acf19ffb23c2d90824fa52d
Sander Pick <sanderpick@gmail.com>
2023-04-17 08:30:09 -0700

    prep migrate to nextjs
    Signed-off-by: Sander Pick <sanderpick@gmail.com>

--- plugins/aos.js
+++
@@ -1,6 +0,0 @@
-import AOS from "aos";
-import "aos/dist/aos.css";
-export default ({ app }) => {
-  // eslint-disable-next-line new-cap
-  app.AOS = new AOS.init({ offset: -150 }); // or any other options you need
-};
