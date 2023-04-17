de832f1641d69827ab41ccfefa6679f91cb15ff3
Sander Pick <sanderpick@gmail.com>
2023-04-17 14:37:07 -0700

    run image op on all rigs
    Signed-off-by: Sander Pick <sanderpick@gmail.com>

--- lib/content.tsx
+++ lib/content.tsx
@@ -36,28 +36,28 @@ export const features: AB<Features> = {
         icon: FaDatabase,
         description:
           "Tableland is a fully-featured, ACID-compliant, relational database built on the blazing-fast SQLite engine.",
-        link: "https://docs.tableland.xyz",
+        link: "https://docs.tableland.xyz/playbooks",
       },
       {
         title: "On-chain security",
         icon: FaLock,
         description:
           "Leverage wallet and contract-based authentication & authorization. Configure tables with fine-grained row- and column-level write access control.",
-        link: "https://docs.tableland.xyz",
+        link: "https://docs.tableland.xyz/fundamentals/architecture/protocol-design",
       },
       {
         title: "Portable",
         icon: FaBriefcase,
         description:
           "Write SQL from your frontend, backend, or smart-contracts.",
-        link: "https://docs.tableland.xyz",
+        link: "https://docs.tableland.xyz/quickstarts",
       },
       {
         title: "Multi-chain",
         icon: FaLink,
         description:
-          "Live on Ethereum, Optimism, Arbitrum, Arbitrum Nova, and Polygon. Filecoin and xkEVMs coming soon.",
-        link: "https://docs.tableland.xyz",
+          "Live on Ethereum, Optimism, Arbitrum, Arbitrum Nova, and Polygon. Filecoin and zkEVMs coming soon.",
+        link: "https://docs.tableland.xyz/fundamentals/chains",
       },
     ],
   },
@@ -69,28 +69,28 @@ export const features: AB<Features> = {
         icon: FaDatabase,
         description:
           "Tableland is a fully-featured, ACID-compliant, relational database built on the blazing-fast SQLite engine.",
-        link: "https://docs.tableland.xyz",
+        link: "https://docs.tableland.xyz/playbooks",
       },
       {
         title: "On-chain security",
         icon: FaLock,
         description:
           "Leverage wallet and contract-based authentication & authorization. Configure tables with fine-grained row- and column-level write access control.",
-        link: "https://docs.tableland.xyz",
+        link: "https://docs.tableland.xyz/fundamentals/architecture/protocol-design",
       },
       {
         title: "Portable",
         icon: FaBriefcase,
         description:
           "Write SQL from your frontend, backend, or smart-contracts.",
-        link: "https://docs.tableland.xyz",
+        link: "https://docs.tableland.xyz/quickstarts",
       },
       {
         title: "Multi-chain",
         icon: FaLink,
         description:
-          "Live on Ethereum, Optimism, Arbitrum, Arbitrum Nova, and Polygon. Filecoin and xkEVMs coming soon.",
-        link: "https://docs.tableland.xyz",
+          "Live on Ethereum, Optimism, Arbitrum, Arbitrum Nova, and Polygon. Filecoin and zkEVMs coming soon.",
+        link: "https://docs.tableland.xyz/fundamentals/chains",
       },
     ],
   },
